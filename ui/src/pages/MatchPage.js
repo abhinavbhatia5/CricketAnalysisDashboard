abcbce49176a02b41a11baac8adf577928f82a33
abhinavbhatia5 <abhi.bhatia03@gmail.com>
2024-09-04 17:46:48 +0530

    Added navigation to matchPage

--- ui/src/pages/MatchPage.js
+++ ui/src/pages/MatchPage.js
@@ -1,54 +1,56 @@
-import {React, useEffect,useState} from 'react';
+import { React, useEffect, useState } from 'react';
 import { MatchDetailComponent } from '../components/MatchDetailComponent';
 import { MatchSmallComponent } from '../components/MatchSmallComponent';
-import { useParams } from 'react-router-dom';
-import './MatchPage.scss' ;
+import { useParams, Link } from 'react-router-dom';
+import './MatchPage.scss';
 import { YearSelector } from '../components/YearSelector';
 
-export const MatchPage=()=>{
+export const MatchPage = () => {
+  const [matches, setMatches] = useState([]);
+  const { teamName, year } = useParams();
 
-    const[matches,setMatches]=useState([]);
-    const {teamName,year}=useParams();
+  useEffect(() => {
+    const fetchMatches = async () => {
+      const response = await fetch(`http://localhost:8082/${teamName}/matches?year=${year}`);
+      const obj = await response.json();
+      console.log(obj);
+      setMatches(obj);
+      //        fetch("http://localhost:8082/team/Chennai Super Kings")
+          //        .then(response=>response.json())
+          //        .then(data=>console.log(data));
+    };
+    fetchMatches();
+  }, [teamName, year]);
 
-    useEffect(
-        ()=>{
-          const fetchMatches=async()=>{
+  if (!matches) {
+    return null;
+  }
 
-    //        fetch("http://localhost:8082/team/Chennai Super Kings")
-    //        .then(response=>response.json())
-    //        .then(data=>console.log(data));
-            const response= await fetch(`http://localhost:8082/${teamName}/matches?year=${year}`);
-                    const obj= await response.json();
-                    console.log(obj);
-                    setMatches(obj);
-          };
-          fetchMatches();
-
-        },[teamName,year]
-      );
-
-if(!matches){
-return null;
-}
-
-
-
-
-return (
+  return (
     <div className="MatchPage">
       <div className="year-selector">
         <h3>Select Year</h3>
-        <YearSelector teamName={teamName}/>
+        <YearSelector teamName={teamName} />
+        <div className="navigation-buttons">
+          <Link to="/">
+            <button>Home-></button>
+          </Link>
+          <Link to={`/team/${teamName}`}>
+            <button>{'<- Team page'}</button>
+          </Link>
+        </div>
       </div>
-      
-      <div className='match-details'>
-      <h1>{teamName} matches in {year}</h1>
+      <div className="match-details">
+        <h1>{teamName} matches in {year}</h1>
 
-        {matches.map((match) => <MatchDetailComponent teamName={teamName} match={match}/>)}
-
+        {matches.length === 0 ? (
+          <p>{`Team did not exist in ${year}`}</p>
+        ) : (
+          matches.map((match) => <MatchDetailComponent key={match.id} teamName={teamName} match={match} />)
+        )}
       </div>
-      
     </div>
   );
-}
-export default MatchPage
+};
+
+export default MatchPage;
